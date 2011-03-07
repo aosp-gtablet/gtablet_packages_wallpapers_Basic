6831f0e839e068c641232e537c86eaca6cb188a9
jason parker <kornyone@gmail.com>
2011-03-07 15:05:48 -0600

    minor tweak of colors

--- src/com/android/wallpaper/nexus/NexusRS.java
+++ src/com/android/wallpaper/nexus/NexusRS.java
@@ -160,10 +160,10 @@ class NexusRS extends RenderScriptScene implements
     private Preset buildColors() {
         final Resources res = mContext.getResources();
 	String colors[] = new String[] {
-	        mPrefs.getString("color0","#66CCDD"),
-                mPrefs.getString("color1","#3366AA"),
-                mPrefs.getString("color2","#6699AA"),
-                mPrefs.getString("color3","#AABBBB")
+	        mPrefs.getString("color0","#FF0000"),
+                mPrefs.getString("color1","#009900"),
+                mPrefs.getString("color2","#0066CC"),
+                mPrefs.getString("color3","#FFCC00")
 	};
         final Preset preset = new Preset(colors);
         return preset;
